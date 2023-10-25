0e38c344bb485a1e22ff71b50cc31c40b9a411cd
drgndenis <serkis-denis.dragan@ogr.iuc.edu.tr>
2023-10-25 23:09:55 +0300

    #53 video is done

---
+++ SwiftUIBootcamp/OnboardingViews/IntroView.swift
@@ -0,0 +1,18 @@
+//
+//  IntroView.swift
+//  SwiftUIBootcamp
+//
+//  Created by Denis DRAGAN on 25.10.2023.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    IntroView()
+}
